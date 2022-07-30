d1d80c2e6c860623c546e2619bc1f5d049173a79
RAJAT GAURAV <53166907+rajatgaurav@users.noreply.github.com>
2022-07-30 19:13:31 +0530

    Update index.js

--- src/components/common/footer/index.js
+++ src/components/common/footer/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./footer.css";
 
 const Footer = () => {
-  return <div className="absolute-center footer">Made with 🔥 by Shivansh</div>;
+  return <div className="absolute-center footer">Made with 🔥 by Rajat Gaurav</div>;
 };
 
 export default Footer;
